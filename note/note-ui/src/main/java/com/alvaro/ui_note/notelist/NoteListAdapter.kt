0634c64678d6b5c3e4a263b134f8a1ab7b0161b9
Alvaro Isea <alvaroe9448@gmail.com>
2022-04-03 18:14:02 +0200

    refactor_5_retrieve_notes note list displayed

--- note/note-ui/src/main/java/com/alvaro/ui_note/notelist/NoteListAdapter.kt
+++ note/note-ui/src/main/java/com/alvaro/ui_note/notelist/NoteListAdapter.kt
@@ -1,67 +1,75 @@
 package com.alvaro.ui_note.notelist
 
-class NoteListAdapter() {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.alvaro.note_domain.model.Note
+import com.alvaro.ui_note.databinding.NoteSingleLayoutBinding
+
+class NoteListAdapter(
+    private val listener: ClickHandler
+) : RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>() {
 
 
-    // private val listener : ClickHandler
-
-    //: RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>()
-
-
-    /*private val differCallBack = object : DiffUtil.ItemCallback<Note>(){
+    private val differCallBack = object : DiffUtil.ItemCallback<Note>() {
         override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
             return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
-            return oldItem.content == newItem.content  && oldItem.title == newItem.title
+            return oldItem.content == newItem.content && oldItem.title == newItem.title
                     && oldItem.timeStamp == newItem.timeStamp
         }
-
     }
 
     val differAsync = AsyncListDiffer(this, differCallBack)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val item = LayoutInflater.from(parent.context).inflate(R.layout.note_single_layout, parent, false)
+        val item = NoteSingleLayoutBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
         return NoteViewHolder(item)
     }
 
     override fun getItemCount(): Int {
-       return differAsync.currentList.size
+        return differAsync.currentList.size
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val note = differAsync.currentList[position]
-        holder.itemView.apply {
-            tvTitle.text = note.title
-            tvContent.text = note.content
-            tvDate.text = note.timeStamp
-            tvPriority.text = note.priority.toString()
-
-
-        }
+        holder.bind(note)
     }
 
-    inner class NoteViewHolder(view :View) : RecyclerView.ViewHolder(view), View.OnClickListener{
+    inner class NoteViewHolder(private val binding: NoteSingleLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
         init {
-            view.setOnClickListener (this)
+            binding.root.setOnClickListener(this)
         }
 
-        override fun onClick(v: View?) {
-             val position = adapterPosition
-            if(position != RecyclerView.NO_POSITION ){
-                listener.itemClick(position)
+        fun bind(note: Note) {
+            binding.apply {
+                tvTitle.text = note.title
+                tvContent.text = note.content
+                tvDate.text = note.timeStamp
+                tvPriority.text = note.priority.toString()
             }
         }
 
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.itemClick(differAsync.currentList[position].id)
+            }
+        }
 
     }
 
-    interface ClickHandler{
-        fun itemClick(position: Int)
-    }*/
-
+    interface ClickHandler {
+        fun itemClick(noteId: Int)
+    }
 
 }
--- note/note-ui/src/main/java/com/alvaro/ui_note/notelist/NoteListFragment.kt
+++ note/note-ui/src/main/java/com/alvaro/ui_note/notelist/NoteListFragment.kt
@@ -2,45 +2,36 @@ package com.alvaro.ui_note.notelist
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.alvaro.ui_note.R
 import com.alvaro.ui_note.databinding.FragmentNoteListBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
-class NoteListFragment : Fragment(R.layout.fragment_note_list) {
+class NoteListFragment : Fragment(R.layout.fragment_note_list), NoteListAdapter.ClickHandler {
 
-    companion object{
+    companion object {
         private const val TAG = "NoteListFragment"
     }
 
-
-    //, NoteListAdapter.ClickHandler
-
-   /* private lateinit var viewModel: NoteViewModel
-    private lateinit var noteAdapter: NoteAdapter
-    private var itemHelper: Any? = null
-    private lateinit var noteList: List<Note>
-    private lateinit var layout : View*/
-
     private lateinit var binding: FragmentNoteListBinding
+    private val viewModel: NoteListViewModel by viewModels()
 
-    private val viewModel : NoteListViewModel by viewModels()
+    private lateinit var noteAdapter: NoteListAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentNoteListBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
 
-        /*layout = view
-        viewModel = (activity as MainActivity).viewModel
-        initRecyclerView()
-        getAllNotes()
-        deleteNote()*/
+        /*deleteNote()*/
 
+        initRecyclerView()
         fabClicked()
         subscribeObservers()
     }
@@ -50,6 +41,7 @@ class NoteListFragment : Fragment(R.layout.fragment_note_list) {
         lifecycleScope.launchWhenStarted {
             viewModel.state.collect { state ->
                 println("${TAG} triggered ${state}")
+                noteAdapter.differAsync.submitList(state.noteList)
             }
         }
 
@@ -58,30 +50,27 @@ class NoteListFragment : Fragment(R.layout.fragment_note_list) {
     private fun fabClicked() {
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_NoteListFragment_to_noteDetailFragment)
-
         }
     }
 
-   /* private fun initRecyclerView() {
-        noteAdapter = NoteAdapter(this)
-        recycler.apply {
+    private fun initRecyclerView() {
+        noteAdapter = NoteListAdapter(this)
+        binding.recycler.apply {
             adapter = noteAdapter
             layoutManager = LinearLayoutManager(activity)
         }
     }
 
 
-
-    private fun getAllNotes() {
+    /*private fun getAllNotes() {
         viewModel.getAllNotes().observe(viewLifecycleOwner, Observer { response ->
             noteList = response
             println("MainFragment, response size -> = ${response.size}")
             noteAdapter.differAsync.submitList(response)
         })
+    }*/
 
-    }
-
-    private fun deleteNote() {
+    /*private fun deleteNote() {
 
         itemHelper = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
@@ -104,19 +93,11 @@ class NoteListFragment : Fragment(R.layout.fragment_note_list) {
 
         val swipeToDelete = ItemTouchHelper(itemHelper as ItemTouchHelper.SimpleCallback)
         swipeToDelete.attachToRecyclerView(recycler)
-    }
-
-    override fun itemClick(position: Int) {
-        val note = noteList[position]
-        Log.d(TAG, "itemClick: priority = ${note.priority} ")
-        val bundle = bundleOf(
-            "id" to note.id,
-            "title" to note.title,
-            "content" to note.content,
-            "priority" to note.priority
-        )
-        NavHelper.navigateWithStack(layout ,R.id.action_mainFragment_to_noteRoomFragment, bundle)
     }*/
+
+    override fun itemClick(noteId: Int) {
+        val bundle = bundleOf( "id" to noteId)
+    }
 
 }
 
